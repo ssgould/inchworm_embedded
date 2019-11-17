9c912559254b82131b0b1f37b602e89578cee4df
tarizzo <33476082+tarizzo@users.noreply.github.com>
2019-11-17 18:43:22 -0500

    remove debugging code

--- src/main.cpp
+++ src/main.cpp
@@ -28,37 +28,10 @@ void setup() {
     motor1.setAngle(45);
     motor2.setAngle(-90);
     motor3.setAngle(5);
-
-    /* Debug */
-    // pinMode(6, OUTPUT);
-    // pinMode(7, OUTPUT);
-    // //pinMode(3, OUTPUT);
-    // servo.attach(3);
 }
 
 void loop() {
-    // motor1.setSpeed(127);
-    // delay(500);
-    // motor1.setSpeed(-127);
-    // delay(500);
-    // analogWrite(3, 200);
-    // digitalWrite(6, HIGH);
-    // digitalWrite(7, LOW);
-    // delay(500);
-    // digitalWrite(6, LOW);
-    // digitalWrite(7, HIGH);
-    // delay(500);
-
     motor1.updateSpeed();
     motor2.updateSpeed();
     motor3.updateSpeed();
-    //delay(1000);
-
-    // digitalWrite(6, HIGH);
-    // digitalWrite(7, LOW);
-    // servo.write(90);
-    // delay(500);
-    // digitalWrite(6, LOW);
-    // digitalWrite(7, HIGH);
-    // delay(500);
 }
