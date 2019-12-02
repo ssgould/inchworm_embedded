6ef3e4e80c9b080f0de0b3d5f65a4060d07ff76d
tarizzo <33476082+tarizzo@users.noreply.github.com>
2019-12-02 14:43:09 -0500

    for josue

--- src/main.cpp
+++ src/main.cpp
@@ -38,22 +38,22 @@ void updateSpeeds();
 Gripper gripper[2];
 
 void setup() {
-    Serial.begin(9600); //Debug Serial
-    Wire.begin(); //begin I2C
+		Serial.begin(9600); //Debug Serial
+		Wire.begin(); //begin I2C
 
-    Serial.println("Robot intializing....");
-    jointMotor[0] = JointMotor(JOINT_MOTOR1_1, JOINT_MOTOR1_2, JOINT_MOTOR1_PWM, JOINT_MOTOR1_ADR, 145, 0.1, 72.5, 10, 0.1, 5);
-    jointMotor[1] = JointMotor(JOINT_MOTOR2_1, JOINT_MOTOR2_2, JOINT_MOTOR2_PWM, JOINT_MOTOR2_ADR, 135, 0.1, 65.5);
-    jointMotor[2] = JointMotor(JOINT_MOTOR3_1, JOINT_MOTOR3_2, JOINT_MOTOR3_PWM, JOINT_MOTOR3_ADR, 10, 0.1, 5, 145, 0.1, 72.5);
+		Serial.println("Robot intializing....");
+		jointMotor[0] = JointMotor(JOINT_MOTOR1_1, JOINT_MOTOR1_2, JOINT_MOTOR1_PWM, JOINT_MOTOR1_ADR, 100, 0.1, 50, 10, 0.1, 5);
+		jointMotor[1] = JointMotor(JOINT_MOTOR2_1, JOINT_MOTOR2_2, JOINT_MOTOR2_PWM, JOINT_MOTOR2_ADR, 120, 0.1, 60);
+		jointMotor[2] = JointMotor(JOINT_MOTOR3_1, JOINT_MOTOR3_2, JOINT_MOTOR3_PWM, JOINT_MOTOR3_ADR, 10, 0.1, 5, 100, 0.1, 60);
 
-    /* DEBUG */
-    jointMotor[0].setAngle(0);
-    jointMotor[1].setAngle(0);
-    jointMotor[2].setAngle(0);
+		/* DEBUG */
+		jointMotor[0].setAngle(0);
+		jointMotor[1].setAngle(0);
+		jointMotor[2].setAngle(0);
 
-    gripper[0] = Gripper(GRIPPER_MOTOR_1, true);
-    gripper[1] = Gripper(GRIPPER_MOTOR_2, false);
-    Serial.println("Done");
+		gripper[0] = Gripper(GRIPPER_MOTOR_1, true);
+		gripper[1] = Gripper(GRIPPER_MOTOR_2, false);
+		Serial.println("Done");
 
 	//Timer1 Interupt
 	// Timer1.initialize(500000);
@@ -63,75 +63,80 @@ void setup() {
 
 void loop() {
 
-    // if(triggerGrip){
-    //     triggerGrip = !gripper[0].setGripper(1, 21000);
-    // }
-    //USED: when want ot enagage and disengage with button
-    //gripperButtonTest(engage, gripper[0], buttonGrip_1);
+	// if(triggerGrip){
+	//     triggerGrip = !gripper[0].setGripper(1, 21000);
+	// }
+	//USED: when want ot enagage and disengage with button
+	//gripperButtonTest(engage, gripper[0], buttonGrip_1);
 
 
-      if (Serial.available() > 0){
-        Serial.println("Message received");
-        Serial.readBytesUntil('\n', serialBuffer, len);
-        int tempIndex = 0;
-        int jointIndex = 0;
+	if (Serial.available() > 0){
+		Serial.println("Message received");
+		Serial.readBytesUntil('\n', serialBuffer, len);
+		Serial.println(serialBuffer[0]);
+		int tempIndex = 0;
+		int jointIndex = 0;
 
-        if (serialBuffer[0] == '-' || serialBuffer[0] == '0') {
-          for(int i = 0; i < len; i++){
-            temp[tempIndex] = serialBuffer[i];
+		if (serialBuffer[0] == '-' || serialBuffer[0] == '0') {
+			for(int i = 0; i < len; i++){
+				temp[tempIndex] = serialBuffer[i];
 
-            if(tempIndex < 2){
-                  tempIndex++;
-            }
-            else{
-                if (jointIndex > 2) { //Gripper
-                  //if((temp[2] - '0') != previousGripperState1){
-                    gripperStatusSerial1 = temp[2] - '0';
-                    previousGripperState1 = gripperStatusSerial1;
-                    gripperFinished1 = false;
-                  //}
+				if(tempIndex < 3){
+					tempIndex++;
+				}
+				else {
+					if (jointIndex > 2) { //Gripper
+						if((temp[2] - '0') != previousGripperState1){ //'0'm = engage
+							gripperStatusSerial1 = temp[2] - '0';
+							previousGripperState1 = gripperStatusSerial1;
+							gripperFinished1 = false;
+						}
 
-                  //if((temp[3] - '0') != previousGripperState2){
-                    gripperStatusSerial2 = temp[3] - '0';
-                    previousGripperState2 = gripperStatusSerial2;
-                    gripperFinished2 = false;
-                  //}
+						if((temp[3] - '0') != previousGripperState2){
+							gripperStatusSerial2 = temp[3] - '0';
+							previousGripperState2 = gripperStatusSerial2;
+							gripperFinished2 = false;
+						}
+						if (temp[2] != temp[3]) {
+							jointMotor[0].switchPID();
+							jointMotor[2].switchPID();
+						}
+					}
+					else { //Joint angles
+						int sign = 1;
+						if (temp[0] == '-') {
+							sign = -1;
+						}
+						temp[0] = '0';
+						jointMotor[jointIndex].setAngle(sign*atoi(temp));
+						jointIndex++;
+						tempIndex = 0;
+					}
+				}
+			}
+		}
+		else {
+			for (int i = 0; i < len; i ++) {
+				Serial.read();
+			}
+		}
+	}
 
-                                    // Serial.print("Gripper 1 (Red): ");
-                                    // Serial.println(gripperStatusSerial1);
-                                    // Serial.print("Gripper 2 (Yellow): ");
-                                    // Serial.println(gripperStatusSerial2);
-                }
-                else { //Joint angles
-                    int sign = 1;
-                    if (temp[0] == '-') {
-                      sign = -1;
-                    }
-                    temp[0] = '0';
-                    jointMotor[jointIndex].setAngle(sign*atoi(temp));
-                    jointIndex++;
-                    tempIndex = 0;
-              }
-            }
-          }
-    }
-  }
+	if(!gripperFinished1){
+		 gripperFinished1 = gripper[0].setGripper(gripperStatusSerial1, 21000);
+		 //Serial.println("Gripper red moving");
+	 }
 
-  if(!gripperFinished1){
-     gripperFinished1 = gripper[0].setGripper(gripperStatusSerial1, 21000);
-     Serial.println("Gripper red moving");
-   }
-
-   if(!gripperFinished2){
-     gripperFinished2 = gripper[1].setGripper(gripperStatusSerial2, 23000);
-          Serial.println("Gripper yellow moving");
-   }
+	if(!gripperFinished2){
+		 gripperFinished2 = gripper[1].setGripper(gripperStatusSerial2, 23000);
+		//Serial.println("Gripper yellow moving");
+	 }
 
 	updateSpeeds();
 
-    // jointMotor[0].updateSpeed();
-    // jointMotor[1].updateSpeed();
-    // jointMotor[2].updateSpeed();
+		// jointMotor[0].updateSpeed();
+		// jointMotor[1].updateSpeed();
+		// jointMotor[2].updateSpeed();
 
 }
 
@@ -151,24 +156,24 @@ void updateSpeeds() {
 */
 void gripperButtonTest(gripperState currentState, Gripper grip, Button buttonGripper){
 
-    if(buttonGripper.isPressed() && buttonGripper.stateChanged() && buttonState){
-      buttonState = false;
-      if(currentState == engage){
-        Serial.println("disengage");
-        grip.setGripper(disengage, 21000);
-      }else{
-        Serial.println("engage");
-        grip.setGripper(engage, 21000);
-      }
-    }
-    if(buttonGripper.isPressed() && buttonGripper.stateChanged() && !buttonState){
-      buttonState = true;
-      if(currentState == engage){
-        Serial.println("engage");
-        grip.setGripper(engage, 21000);
-      }else{
-        Serial.println("disengage");
-        grip.setGripper(disengage, 21000);
-      }
-    }
+		if(buttonGripper.isPressed() && buttonGripper.stateChanged() && buttonState){
+			buttonState = false;
+			if(currentState == engage){
+				Serial.println("disengage");
+				grip.setGripper(disengage, 21000);
+			}else{
+				Serial.println("engage");
+				grip.setGripper(engage, 21000);
+			}
+		}
+		if(buttonGripper.isPressed() && buttonGripper.stateChanged() && !buttonState){
+			buttonState = true;
+			if(currentState == engage){
+				Serial.println("engage");
+				grip.setGripper(engage, 21000);
+			}else{
+				Serial.println("disengage");
+				grip.setGripper(disengage, 21000);
+			}
+		}
 }
